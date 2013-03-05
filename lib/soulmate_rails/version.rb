e675815672c2543235653eede53a8e394372e1d5
Dhruva Sagar <dhruva.sagar@gmail.com>
2013-03-05 15:51:47 +0530

    First stable release 0.2.1

--- lib/soulmate_rails/version.rb
+++ lib/soulmate_rails/version.rb
@@ -2,7 +2,6 @@ module SoulmateRails
   MAJOR = 0
   MINOR = 2
   PATCH = 1
-  STATUS = 'beta'
 
-  VERSION = [MAJOR, MINOR, PATCH, STATUS].compact.join('.')
+  VERSION = [MAJOR, MINOR, PATCH].compact.join('.')
 end
